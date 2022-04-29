09fb1f8d9b42bc63bd2586cc3e4ee44fd418f197
Wirex29 <nguyenminh012326@gmail.com>
2022-04-29 20:14:00 +0700

    Update

--- auth/apiAuthentication.js
+++
@@ -1,9 +0,0 @@
-const passport = require("passport");
-
-module.exports = function (req, res, next) {
-    return passport.authenticate("login", function (err, user) {
-        if (err) return next(err);
-        if (user) req.user = user;
-        next();
-    })(req, res, next);
-};
