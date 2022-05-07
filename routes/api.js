5759941c65d8cf20dcf826be9bda9c1507afa63c
Wirex29 <nguyenminh012326@gmail.com>
2022-05-07 11:40:55 +0700

    fix

--- routes/api.js
+++ routes/api.js
@@ -332,6 +332,7 @@ function checkAuthenticated(req, res, next) {
     if (!req.user) {
         return res.status(401).send({errorMessage: "Not logged in."})
     }
+    console.log(req.headers)
     next()
 }
 
